a07ca8178589d6b8718c29ff38eb6680959a5e3f
nuri <nooti777712@gmail.com>
2019-03-19 14:39:52 -0700

    Create Slideable and Stepable module

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,17 +1,20 @@
+require
+
 class Piece
   def initialize(color, board, pos)
     @color = color
     @board = board
     @pos = pos
   end
+
+  def moves
+    return []
+  end
 end
 
 class NullPiece < Piece
   def initialize(color, board, pos)
     super(color, board, pos)
-  end
-
-  def moves
   end
 
   def symbol
